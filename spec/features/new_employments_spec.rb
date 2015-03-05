2a334fa73a97cad0006a96674511f6bbf84d1950
Tristan Rubadeau <tristan@rubadeau.com>
2015-03-05 14:46:44 -0700

    green org test

--- spec/features/new_employments_spec.rb
+++ spec/features/new_employments_spec.rb
@@ -3,16 +3,42 @@ require 'rails_helper'
 feature 'New Employments' do
 
   scenario 'User can create a new employment' do
+
+    person = Person.create!(
+    first_name: "Firsty",
+    last_name: "Lasty"
+    )
+
     visit employments_path
     expect(page).to have_content('Employments')
     click_on 'New Employment'
 
     expect(page).to have_content('New Employment')
-    select person.full_name, from: :toy_cat_id
-    select cat.name, from: :toy_cat_id
-    select cat.name, from: :toy_cat_id
-    select cat.name, from: :toy_cat_id
+    select person.full_name, from: :employment_person_id
+
+  end
+
+  scenario 'User can create a new employment' do
+
+    organization = Organization.create!(
+    name: "gSchool"
+    )
+
+    visit employments_path
+    expect(page).to have_content('Employments')
+    click_on 'New Employment'
+
+    expect(page).to have_content('New Employment')
+    select organization.name, from: :employment_organization_id
     click_on 'Create Employment'
-    expect(page).to have_content(employment.name)
+
   end
 end
+
+
+
+# select organization.name, from: :employment_organization_id
+# select job_title.name, from: :employment_job_title_id
+# select location.name, from: :employment_location_id
+
+# expect(page).to have_content(employment.name)
