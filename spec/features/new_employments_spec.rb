8b78cc5b7ff5241bf48de36bff825db233bdfbdf
Tristan Rubadeau <tristan@rubadeau.com>
2015-03-05 15:00:23 -0700

    green location spec

--- spec/features/new_employments_spec.rb
+++ spec/features/new_employments_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'New Employments' do
 
-  scenario 'User can create a new employment' do
+  scenario 'User can select person' do
 
     person = Person.create!(
     first_name: "Firsty",
@@ -18,7 +18,7 @@ feature 'New Employments' do
 
   end
 
-  scenario 'User can create a new employment' do
+  scenario 'User can select a organization' do
 
     organization = Organization.create!(
     name: "gSchool"
@@ -30,9 +30,36 @@ feature 'New Employments' do
 
     expect(page).to have_content('New Employment')
     select organization.name, from: :employment_organization_id
-    click_on 'Create Employment'
 
   end
+  scenario 'User can select a job title' do
+
+    job_title = JobTitle.create!(
+    name: "CEO"
+    )
+
+    visit employments_path
+    expect(page).to have_content('Employments')
+    click_on 'New Employment'
+
+    expect(page).to have_content('New Employment')
+    select job_title.name, from: :employment_job_title_id
+  end
+
+  scenario 'User can add a location' do
+
+    location = Location.create!(
+    name: "Omaha"
+    )
+
+    visit employments_path
+    expect(page).to have_content('Employments')
+    click_on 'New Employment'
+
+    expect(page).to have_content('New Employment')
+    select location.name, from: :employment_location_id
+  end
+
 end
 
 
